c7e020b3261d460604e609a72543792fed77bc52
raul <develop.raul@gmail.com>
2022-06-08 19:28:48 +0900

    [FIX] 각Service Interface에 대해 Reactor도입을 고려하여 스펙 수정

--- src/main/kotlin/site/iplease/gatewayserver/service/AuthTokenService.kt
+++ src/main/kotlin/site/iplease/gatewayserver/service/AuthTokenService.kt
@@ -1,11 +1,12 @@
 package site.iplease.gatewayserver.service
 
 import org.springframework.http.HttpHeaders
+import reactor.core.publisher.Mono
 import site.iplease.gatewayserver.data.dto.AccountDto
 
 interface AuthTokenService {
-    fun extractToken(headers: HttpHeaders): String
-    fun validToken(token: String): Boolean
-    fun decodeToken(token: String): AccountDto
-    fun containToken(headers: HttpHeaders): Boolean
+    fun extractToken(headers: HttpHeaders): Mono<String>
+    fun validToken(token: String): Mono<Boolean>
+    fun decodeToken(token: String): Mono<AccountDto>
+    fun containToken(headers: HttpHeaders): Mono<Boolean>
 }
--- src/main/kotlin/site/iplease/gatewayserver/service/ProfileService.kt
+++ src/main/kotlin/site/iplease/gatewayserver/service/ProfileService.kt
@@ -1,8 +1,9 @@
 package site.iplease.gatewayserver.service
 
+import reactor.core.publisher.Mono
 import site.iplease.gatewayserver.data.response.ProfileResponse
 
 interface ProfileService {
-    fun getProfileByAccessToken(accessToken: String): ProfileResponse
-    fun existProfileByAccessToken(token: String): Boolean
+    fun getProfileByAccessToken(accessToken: String): Mono<ProfileResponse>
+    fun existProfileByAccessToken(accessToken: String): Mono<Boolean>
 }
