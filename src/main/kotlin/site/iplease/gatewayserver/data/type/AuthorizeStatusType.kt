602d782ef32a0f4b400172adc8acbb85325b8014
raul <develop.raul@gmail.com>
2022-06-09 08:55:23 +0900

    [ADD] AuthorizeStatusType에 접근권한이 없을 경우에 대한 상태 추가

--- src/main/kotlin/site/iplease/gatewayserver/data/type/AuthorizeStatusType.kt
+++ src/main/kotlin/site/iplease/gatewayserver/data/type/AuthorizeStatusType.kt
@@ -1,5 +1,5 @@
 package site.iplease.gatewayserver.data.type
 
 enum class AuthorizeStatusType {
-    SUCCESS, EMPTY_TOKEN, WRONG_TOKEN, UNKNOWN_ERROR
+    SUCCESS, EMPTY_TOKEN, WRONG_TOKEN, UNKNOWN_ERROR, PERMISSION_DENIED
 }
