ed1d951c6b9e9295c2a0fa739132c0ef07c929e8
raul <develop.raul@gmail.com>
2022-06-08 20:49:53 +0900

    [FIX] 직렬화를 위한 ObjectMapper 빈 등록

--- src/main/kotlin/site/iplease/gatewayserver/config/WebClientConfig.kt
+++ src/main/kotlin/site/iplease/gatewayserver/config/WebClientConfig.kt
@@ -1,5 +1,8 @@
 package site.iplease.gatewayserver.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,4 +12,9 @@ import org.springframework.web.reactive.function.client.WebClient
 class WebClientConfig {
     @Bean @LoadBalanced
     fun loadBalancedWebClientBuilder() = WebClient.builder()
+
+    @Bean
+    fun objectMapper(): ObjectMapper = ObjectMapper()
+        .registerKotlinModule()
+        .registerModule(JavaTimeModule())
 }
