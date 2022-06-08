88c8e1e82f8f6e846cb344704454a2210b39c6be
raul <develop.raul@gmail.com>
2022-06-08 19:29:19 +0900

    [FIX] Interface의 스펙 수정에 따른 구현체 변경

--- src/main/kotlin/site/iplease/gatewayserver/service/JwtAuthTokenService.kt
+++ src/main/kotlin/site/iplease/gatewayserver/service/JwtAuthTokenService.kt
@@ -2,6 +2,7 @@ package site.iplease.gatewayserver.service
 
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
+import reactor.kotlin.core.publisher.toMono
 import site.iplease.gatewayserver.data.dto.AccountDto
 import site.iplease.gatewayserver.data.response.ProfileResponse
 
@@ -9,11 +10,19 @@ import site.iplease.gatewayserver.data.response.ProfileResponse
 class JwtAuthTokenService(
     private val profileService: ProfileService
 ): AuthTokenService {
-    override fun containToken(headers: HttpHeaders) = headers.containsKey(HttpHeaders.AUTHORIZATION)
-        .let { if(it) headers[HttpHeaders.AUTHORIZATION]!![0].contains("Bearer ") else false }
-    override fun extractToken(headers: HttpHeaders) = headers.getOrEmpty(HttpHeaders.AUTHORIZATION)[0].substring("Bearer ".length)
-    override fun validToken(token: String) = profileService.existProfileByAccessToken(token)
-    override fun decodeToken(token: String): AccountDto = profileService.getProfileByAccessToken(accessToken = token).toAccountDto()
+    override fun containToken(headers: HttpHeaders) =
+        Unit.toMono()
+            .map { headers.containsKey(HttpHeaders.AUTHORIZATION) }
+            .map { containAuthHeader -> if(containAuthHeader) headers[HttpHeaders.AUTHORIZATION]!![0].contains("Bearer ") else false }
+    override fun extractToken(headers: HttpHeaders) =
+        Unit.toMono()
+            .map { headers.getOrEmpty(HttpHeaders.AUTHORIZATION)[0] }
+            .map { it.substring("Bearer ".length) }
+    override fun validToken(token: String) =
+        profileService.existProfileByAccessToken(token)
+    override fun decodeToken(token: String) =
+        profileService.getProfileByAccessToken(accessToken = token)
+            .map { it.toAccountDto() }
 }
 
 private fun ProfileResponse.toAccountDto(): AccountDto = AccountDto(accountId = this.common.accountId, permission = this.common.permission)
