2b8ce1b65b3340c803218c7c5525d539f342ae6f
raul <develop.raul@gmail.com>
2022-08-07 17:08:15 +0900

    [ADD] CORS Config 재작성

---
+++ src/main/kotlin/site/iplease/gatewayserver/config/GlobalCorsConfig.kt
@@ -0,0 +1,17 @@
+package site.iplease.gatewayserver.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+@EnableWebFlux
+class GlobalCorsConfig : WebFluxConfigurer {
+    override fun addCorsMappings(corsRegistry: CorsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .maxAge(3600)
+    }
+}
